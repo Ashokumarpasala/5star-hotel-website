7997942475ef63599fa0e12910a80a61ea1836ff
Ashokumarpasala <adinarayanapasala9Wgmail.com>
2023-11-20 15:10:25 +0530

    first commit

---
+++ assets/js/switchRooms.js
@@ -0,0 +1,6 @@
+
+const hamburger = document.querySelector('.header-hamburger-icon');
+const standardRoomSelector = document.querySelector('#standard-room')
+const executiveRoomSelector = document.querySelector('#executive-room')
+const kingRoomSelector = document.querySelector('#king-room')
+
